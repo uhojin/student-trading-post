42b7f4ae14516d331c80b514f263387f4dc8845b
Matthew Pimentel <pimatthe@sheridancollege.ca>
2023-04-12 18:51:29 -0400

    Chats are now being stored in the database

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:student_trade_post_app/screens/Chat.dart';
-void main() {
-  runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/Chat.dart
+++ lib/screens/Chat.dart
@@ -1,18 +1,122 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Chat extends StatefulWidget {
-  const Chat({super.key});
+  const Chat({Key? key}) : super(key: key);
 
   @override
-  State<Chat> createState() => _ChatState();
+  _ChatState createState() => _ChatState();
 }
 
 class _ChatState extends State<Chat> {
+  late String _userId;
+  late CollectionReference _messagesCollection;
+  final TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    Firebase.initializeApp().then((value) {
+      FirebaseAuth.instance.signInAnonymously().then((userCredential) {
+        setState(() {
+          _userId = userCredential.user!.uid;
+          _messagesCollection = FirebaseFirestore.instance.collection('messages');
+        });
+      });
+    });
+  }
+
+  Future<void> _sendMessage(String content) async {
+    if (content.trim().isNotEmpty) {
+      await _messagesCollection.add({
+        'content': content,
+        'idFrom': _userId,
+        'idTo': '',
+        'timestamp': DateTime.now().millisecondsSinceEpoch.toString(),
+      });
+      _textEditingController.clear();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hello"),
+      appBar: AppBar(
+        title: const Text('Chat App'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (BuildContext context, AsyncSnapshot<FirebaseApp> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return Column(
+              children: [
+                Expanded(
+                  child: StreamBuilder<QuerySnapshot>(
+                    stream: _messagesCollection.orderBy('timestamp').snapshots(),
+                    builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasError) {
+                        return const Text('Something went wrong');
+                      }
+
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Text('Loading...');
+                      }
+
+                      final List<QueryDocumentSnapshot> messages = snapshot.data!.docs
+                          .where((doc) =>
+                      //UPDATE THE aaa TO USER ID OF POST
+                      (doc['idTo'] == _userId && doc['idFrom'] == "aaa") ||
+                          (doc['idTo'] == "aaa" && doc['idFrom'] == _userId))
+                          .toList();
+
+                      return Expanded(
+                          child: ListView.builder(
+                          itemCount: messages.length,
+                          itemBuilder: (BuildContext context, int index) {
+                        final message = messages[index];
+                        return ListTile(
+                          title: Text(message['content']),
+                          );
+                        },
+                          ),
+                      );
+                    },
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: _textEditingController,
+                          decoration: const InputDecoration(
+                            hintText: 'Type a message',
+                            contentPadding: EdgeInsets.all(16),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.send),
+                        onPressed: () {
+                          _sendMessage(_textEditingController.text.trim());
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }
+        },
       ),
     );
   }
