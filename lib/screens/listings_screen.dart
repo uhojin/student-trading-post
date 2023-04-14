31f1a2463efafdbbf4fdf50cd05889453bace678
Hojin You <youho@sheridancollege.ca>
2023-04-13 21:03:48 -0400

    Added bottom nav bar function

--- lib/screens/listings_screen.dart
+++ lib/screens/listings_screen.dart
@@ -17,7 +17,6 @@ class ListingsScreen extends StatefulWidget {
 class _ListingsScreenState extends State<ListingsScreen> {
   int _currentIndex = 0;
 
-  final List<Widget> _tabs = [    const ListingsScreen(),    const ChatList(),    const FavouritesScreen(),  ];
 
   void _onTabTapped(int index) {
     setState(() {
@@ -30,7 +29,7 @@ class _ListingsScreenState extends State<ListingsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: const Icon(Icons.menu),
+        // leading: const Icon(Icons.menu),
 
         title: const Text('Listings',
           style: TextStyle(
@@ -42,105 +41,108 @@ class _ListingsScreenState extends State<ListingsScreen> {
 
       ),
       body: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance.collection('listings').snapshots(),
-          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            }
-            List<QueryDocumentSnapshot> listings = snapshot.data!.docs;
+        stream: FirebaseFirestore.instance.collection('listings').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          List<QueryDocumentSnapshot> listings = snapshot.data!.docs;
 
-            return AppinioSwiper(
-              loop: true,
-              cardsBuilder: (BuildContext context, int index) {
-                // Extract listing data from snapshot
-                String title = listings[index]['title'];
-                String description = listings[index]['description'];
-                bool isFree = listings[index]['isFree'];
-                List<dynamic> imageUrls = listings[index]['imageUrls'];
+          return IndexedStack(
+            index: _currentIndex,
+            children: [
+              AppinioSwiper(
+                  loop: true,
+                  cardsBuilder: (BuildContext context, int index) {
+                    // Extract listing data from snapshot
+                    String title = listings[index]['title'];
+                    String description = listings[index]['description'];
+                    bool isFree = listings[index]['isFree'];
+                    List<dynamic> imageUrls = listings[index]['imageUrls'];
 
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ListingDetailsScreen(
-                          title: title,
-                          description: description,
-                          imageUrls: listings[index]['imageUrls'],
-                          documentId: listings[index].id,
-                          userId: listings[index]['userId'],
-                          isFree: listings[index]['isFree'],
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ListingDetailsScreen(
+                              title: title,
+                              description: description,
+                              imageUrls: listings[index]['imageUrls'],
+                              documentId: listings[index].id,
+                              userId: listings[index]['userId'],
+                              isFree: listings[index]['isFree'],
+                            ),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Card(
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: MediaQuery.of(context).size.height *
+                                    0.5, // Set a minimum height
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: Image.network(
+                                    imageUrls[0],
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      title,
+                                      style: const TextStyle(
+                                        fontSize: 24.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    isFree
+                                        ? const Text(
+                                      'Free',
+                                      style: TextStyle(
+                                        color: Colors.green,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    )
+                                        : const Text(
+                                      'Trade',
+                                      style: TextStyle(
+                                        color: Colors.blue,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 16.0),
+                                    Text(
+                                      description,
+                                      style: const TextStyle(fontSize: 18.0),
+                                      // overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     );
                   },
-                  child: Container(
-                    alignment: Alignment.center,
-                    child: Card(
-
-                      child: Column(
-
-                        children: [
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.5, // Set a minimum height
-                            child: Padding(
-                              padding: const EdgeInsets.all(8),
-                              child: Image.network(
-                                imageUrls[0],
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Column(
-                              // mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  title,
-                                  style: const TextStyle(
-
-                                    fontSize: 24.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                isFree
-                                    ? const Text(
-                                  'Free',
-                                  style: TextStyle(
-                                    color: Colors.green,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                )
-                                    : const Text(
-                                  'Trade',
-                                  style: TextStyle(
-                                    color: Colors.blue,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                const SizedBox(height: 16.0),
-                                Text(
-                                  description,
-                                  style: const TextStyle(fontSize: 18.0),
-                                  // overflow: TextOverflow.ellipsis,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              }, cardsCount: listings.length,);
-
-          }
-
+                  cardsCount: listings.length),
+              const ChatList(),
+              const FavouritesScreen(),
+            ],
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -154,6 +156,24 @@ class _ListingsScreenState extends State<ListingsScreen> {
         child: const Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Listings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+        ],
+      ),
     );
   }
 }
